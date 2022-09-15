79b4de9586ec22b118e524f94506f18efe666ca7
dddrbz <danton987@gmail.com>
2022-09-15 03:10:57 +0300

    fix Push annotation

---
+++ src/test/java/dddrbz/SimpleChat/SimpleChat/SimpleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dddrbz.SimpleChat.SimpleChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
